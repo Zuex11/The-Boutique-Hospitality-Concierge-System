0afd37e1bac7bac5d0a1d820b658fdf76b0f391c
yomna33 <yomnayomna254@gmail.com>
2026-05-10 20:17:15 +0300

    add DatabaseConnection

---
+++ src/db/DatabaseConnection.java
@@ -0,0 +1,34 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+     private static final String URL =
+        "jdbc:sqlserver://localhost\\SQLEXPRESS;databaseName=Database_project"
+        + ";integratedSecurity=true;encrypt=true;trustServerCertificate=true";
+
+    private static DatabaseConnection instance ;
+    private Connection connection;
+
+    private DatabaseConnection () throws SQLException {
+        this.connection = DriverManager.getConnection(URL); //takes url and opens a connection with the sql server
+    }
+
+    private DatabaseConnection getInstance () throws SQLException {
+        if (instance == null || instance.connection.isClosed()) { //avoids opening two connections
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public connection getConnection(){
+        return connection;
+    }
+
+
+      
+
+}
