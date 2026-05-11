424a3c58eb3595384015fe810dac66dbf2bd8387
Yusuf-El-sheikh <yusuf.elsheikh01@gmail.com>
2026-05-11 21:44:29 +0300

    suite class edit

--- src/models/SuiteClass.java
+++ src/models/SuiteClass.java
@@ -2,18 +2,22 @@ package models;
 
 public class SuiteClass {
     private int classId;
+    private int hotelId;
     private String className;
     private double nightlyRate;
     private String amenities;
 
-    public SuiteClass(String className, double nightlyRate, String amenities) {
+    public SuiteClass(int hotelId, String className, double nightlyRate, String amenities) {
+        this.hotelId = hotelId;
         this.className = className;
         this.nightlyRate = nightlyRate;
         this.amenities = amenities;
     }
 
     public int getClassId() { return classId; }
-
+    public void setClassId(int classId) { this.classId = classId; }
+    public int getHotelId() { return hotelId; }
+    public void setHotelId(int hotelId) { this.hotelId = hotelId; }
     public String getClassName() { return className; }
     public void setClassName(String className) { this.className = className; }
 
